a10c0b558171f649892f64c02dc7b2e4b4a3c48d
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-04-07 15:25:07 +0500

    new code

---
+++ task1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter number: ");
+            int number2 = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number2; i++)
+            {
+                for (int j = number2; j >= 1; j--)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write("* ");
+                    }
+                    else { Console.Write(" "); }
+
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = number2; i >= 1; i--)
+            {
+                for (int j = number2; j >= 2; j--)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write(" *");
+                    }
+                    else { Console.Write(" "); }
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+     }
+    }
